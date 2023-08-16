63d5577cb5446d26edb8f13bae6d148afb2d7244
황준혁 <strawji02@naver.com>
2023-08-16 15:38:31 +0900

    feat: :lipstick: update tabsButton component style props
    Tabs 컴포넌트의 style 전용 prop를 추가했습니다

--- src/components/base/Tabs/TabsBody.tsx
+++ src/components/base/Tabs/TabsBody.tsx
@@ -1,19 +1,24 @@
 import { DEFAULT_PROPS_PREFIX } from '@/utils/constants';
-import { HTMLAttributes } from 'react';
+import { CSSProperties, HTMLAttributes } from 'react';
 import { TabValueType } from './TabsMain';
 
 export interface TabsBodyProps extends HTMLAttributes<HTMLDivElement> {
   value: TabValueType;
+  bgColor?: CSSProperties['backgroundColor'];
 }
 
 export const tabsBodyType = 'tabs-body';
 
-function TabsBody({ value, ...props }: TabsBodyProps) {
-  return <div css={{}} {...props} />;
+function TabsBody({
+  value,
+  bgColor: backgroundColor,
+  ...props
+}: TabsBodyProps) {
+  return <div css={{ backgroundColor }} {...props} />;
 }
 
 TabsBody.defaultProps = {
   [DEFAULT_PROPS_PREFIX]: tabsBodyType,
 };
 
-export default TabsBody;
+export default TabsBody; //
