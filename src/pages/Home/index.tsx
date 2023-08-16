5f5a3f0cf290ccfdf88eb4b876ab060022549850
황준혁 <strawji02@naver.com>
2023-08-16 15:39:23 +0900

    feat: add home component
    Home 컴포넌트를 추가했습니다

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,12 @@
 import { Stack } from '../../components/base';
-import { Banner } from './Banner';
+import Banner from './Banner';
 
 export interface HomeProps {}
 
 function Home({ ...props }: HomeProps) {
   return (
     <Stack>
-      <Banner>
-        <Banner.Slide>hi</Banner.Slide>
-        <Banner.Slide>hi</Banner.Slide>
-        <Banner.Slide>hi</Banner.Slide>
-        <Banner.Slide>hi</Banner.Slide>
-        <Banner.Slide>hi</Banner.Slide>
-        <div>hihi</div>
-      </Banner>
+      <Banner />
     </Stack>
   );
 }
