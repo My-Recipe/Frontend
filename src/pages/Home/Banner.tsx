2635065fb247ef3c345ec97e8fd8cad22617c592
황준혁 <strawji02@naver.com>
2023-08-16 15:39:23 +0900

    feat: add home component
    Home 컴포넌트를 추가했습니다

---
+++ src/pages/Home/Banner.tsx
@@ -0,0 +1,43 @@
+import { Tabs } from '@base/Tabs';
+import { TabValueType } from '@base/Tabs/TabsMain';
+import { css } from '@emotion/react';
+import { useState } from 'react';
+
+export interface BannerProps {}
+
+function Banner({ ...props }: BannerProps) {
+  const [value, setValue] = useState<TabValueType>(1);
+  const styleProps = {
+    buttonGroupCss: css({}),
+    buttonCss: css({}),
+    bodyWrapperCss: css({}),
+    bodyCss: css({ height: 680, width: '100%' }),
+    css: {
+      padding: '0 20px',
+    },
+  };
+  return (
+    <>
+      <Tabs
+        {...styleProps}
+        value={value}
+        onTabChange={(value) => setValue(value)}
+      >
+        <Tabs.Button bgColor="#FF874F" value={1}>
+          01
+        </Tabs.Button>
+        <Tabs.Button bgColor="#FFEB3A" value={2}>
+          02
+        </Tabs.Button>
+        <Tabs.Body bgColor="#FF874F" value={1}>
+          세상에 똑같은 레시피느ㄴ없다
+        </Tabs.Body>
+        <Tabs.Body bgColor="#FFEB3A" value={2}>
+          tast2body
+        </Tabs.Body>
+      </Tabs>
+    </>
+  );
+}
+
+export default Banner;
