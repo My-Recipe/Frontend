f434d3ac1f814eb1e8b039a42ed337211528e9d4
황준혁 <strawji02@naver.com>
2023-08-17 11:39:50 +0900

    chore: :truck: update tabs component
    Tabs 컴포넌트의 export를 index.ts에 추가하고, defaulValue의 값이 적용되지 않던 버그를 수정했습니다

--- src/components/base/Tabs/index.ts
+++ src/components/base/Tabs/index.ts
@@ -2,7 +2,9 @@ import TabsBody from './TabsBody';
 import TabsButton from './TabsButton';
 import TabsMain from './TabsMain';
 
-export const Tabs = Object.assign(TabsMain, {
+const Tabs = Object.assign(TabsMain, {
   Body: TabsBody,
   Button: TabsButton,
 });
+
+export default Tabs;
