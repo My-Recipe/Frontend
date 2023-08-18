3d9a13d0b798a90668cd754f713bfacf4e60a3b3
황준혁 <strawji02@naver.com>
2023-08-19 01:05:47 +0900

    chore: :fire: remove commnet
    주석 삭제, page index에 예시 버튼 추가했습니다

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import { useRoutes } from 'react-router-dom';
 import routes from '~react-pages';
 
 function App() {
-  return (
-    <Suspense fallback={<p>Loading...</p>}>
-      {useRoutes(routes)}
-      {/* <RouterProvider router={router} /> */}
-    </Suspense>
-  );
+  return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>;
 }
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+
 export interface IndexProps {}
 
 function Index({ ...props }: IndexProps) {
-  return <>Home</>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <button
+        onClick={() => {
+          navigate('/home');
+        }}
+      >
+        go to home
+      </button>
+    </>
+  );
 }
 
 export default Index;
