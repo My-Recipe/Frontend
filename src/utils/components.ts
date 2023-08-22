30bbb6838d2cf172b818ebcce83e8cc9752ebc34
황준혁 <strawji02@naver.com>
2023-08-22 15:13:45 +0900

    chore: :sparkles: add mergeRef util
    ref 여러개를 인자로 받아 하나의 ref를 리턴해주는 mergeRef 함수를 추가했습니다

--- src/utils/components.ts
+++ src/utils/components.ts
@@ -1,4 +1,10 @@
-import { Children, ReactNode, isValidElement } from 'react';
+import {
+  Children,
+  MutableRefObject,
+  ReactNode,
+  RefCallback,
+  isValidElement,
+} from 'react';
 import { DEFAULT_PROPS_PREFIX } from './constants';
 
 export function getComponentFromType(children: ReactNode, type: string) {
@@ -7,8 +13,23 @@ export function getComponentFromType(children: ReactNode, type: string) {
     (child) =>
       isValidElement(child) &&
       (child.props[DEFAULT_PROPS_PREFIX] === type ||
+        //emotion 자체 defaultProps 추가로 인해 1depth 더 검사
         child.props['__EMOTION_TYPE_PLEASE_DO_NOT_USE__']?.defaultProps?.[
           DEFAULT_PROPS_PREFIX
         ] === type),
   );
+}
+
+export function mergeRef<T>(
+  ...refs: Array<MutableRefObject<T> | RefCallback<T>>
+) {
+  return (element: T) => {
+    refs.forEach((ref) => {
+      if (typeof ref === 'function') {
+        ref(element);
+      } else {
+        ref.current = element;
+      }
+    });
+  };
 }
