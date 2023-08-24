a43514a3ec63fbae654c1ee164712c7712d6517b
황준혁 <strawji02@naver.com>
2023-08-24 11:54:44 +0900

    feat: :sparkles: add inputbox component
    inputBox 컴포넌트를 추가했습니다. Chip 제작 후 추가적인 작업이 필요합니다.

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -1,5 +1,7 @@
-import IconSearch from '@/assets/icon_search_xs.svg';
+import IconSearch from '@/assets/icon_search.svg';
+import IconSearchXs from '@/assets/icon_search_xs.svg';
 import { Group, Popover, Stack } from '@base';
+import hangul from 'hangul-js';
 import { useState } from 'react';
 
 export interface InputBoxProps {
@@ -9,26 +11,34 @@ export interface InputBoxProps {
 function InputBox({ searchItems, ...props }: InputBoxProps) {
   const [inputValue, setInputValue] = useState('');
 
-  const filterdSearchItems = searchItems?.filter((itemValue) =>
-    itemValue.includes(inputValue),
+  const filterdSearchItems = searchItems?.filter(
+    (itemValue) => hangul.search(itemValue, inputValue) >= 0,
   );
 
   return (
     <Popover preventCloseOnClickTrigger position="bottom-left">
       <Popover.Trigger>
-        <input
-          onChange={(e) => setInputValue(e.target.value)}
-          value={inputValue}
-          css={{ width: '100%' }}
-          type="text"
-        />
+        <div css={{ padding: '16px 26px', backgroundColor: 'white' }}>
+          <Group css={{ gap: 13 }}>
+            <img src={IconSearch} />
+            <input
+              onChange={(e) => setInputValue(e.target.value)}
+              value={inputValue}
+              css={{ color: 'black', flex: 1 }}
+              type="text"
+            />
+          </Group>
+        </div>
       </Popover.Trigger>
       <Popover.Content>
         {filterdSearchItems && (
           <Stack spacing={10}>
-            {filterdSearchItems.map((itemValue) => (
-              <Group css={{ gap: 10 }}>
-                <img src={IconSearch} />
+            {filterdSearchItems.map((itemValue, index) => (
+              <Group
+                key={`search-item-${itemValue}-${index}`}
+                css={{ gap: 10 }}
+              >
+                <img src={IconSearchXs} />
                 {itemValue}
               </Group>
             ))}
