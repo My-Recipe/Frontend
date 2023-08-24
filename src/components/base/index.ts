1e3792d263bcc7d26f1ea20c8ed5ae06db679ac8
황준혁 <strawji02@naver.com>
2023-08-24 18:43:20 +0900

    chore: :pencil2: update Typography index export
    base 컴포넌트에서 Typography export를 추가했습니다

--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -1,4 +1,5 @@
-export { default as Group } from './Group.tsx';
+export { default as Group } from './Group';
 export { default as Popover } from './Popover';
-export { default as Stack } from './Stack.tsx';
+export { default as Stack } from './Stack';
 export { default as Tabs } from './Tabs';
+export { default as Typography } from './Typography';
