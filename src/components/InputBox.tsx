c25fae07211f29447653eb3ae75e9c1cc47c1bc0
황준혁 <strawji02@naver.com>
2023-08-25 16:50:42 +0900

    refactor: :zap: update InputBox component value & onChange props
    InputBox 컴포넌트에 value와 onChange props를 추가했습니다

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -2,18 +2,25 @@ import IconSearchXs from '@/assets/icon-search-xs.svg';
 import IconSearch from '@/assets/icon-search.svg';
 import { Group, Popover, Stack } from '@base';
 import hangul from 'hangul-js';
-import { useState } from 'react';
+import { useMemo } from 'react';
 
 export interface InputBoxProps {
   searchItems?: string[];
+  value: string;
+  onChange: (value: string) => void;
 }
 
-function InputBox({ searchItems, ...props }: InputBoxProps) {
-  const [inputValue, setInputValue] = useState('');
-
+function InputBox({
+  value: inputValue,
+  onChange,
+  searchItems,
+  ...props
+}: InputBoxProps) {
   const filterdSearchItems = searchItems?.filter(
     (itemValue) => hangul.search(itemValue, inputValue) >= 0,
   );
+
+  const iconSearchXs = useMemo(() => <img src={IconSearchXs} />, []);
 
   return (
     <Popover preventCloseOnClickTrigger position="bottom-left">
@@ -22,7 +29,7 @@ function InputBox({ searchItems, ...props }: InputBoxProps) {
           <Group gap={13}>
             <img src={IconSearch} />
             <input
-              onChange={(e) => setInputValue(e.target.value)}
+              onChange={(e) => onChange(e.target.value)}
               value={inputValue}
               css={{ color: 'black', flex: 1 }}
               type="text"
@@ -35,7 +42,7 @@ function InputBox({ searchItems, ...props }: InputBoxProps) {
           <Stack spacing={10}>
             {filterdSearchItems.map((itemValue, index) => (
               <Group gap={10} key={`search-item-${itemValue}-${index}`}>
-                <img src={IconSearchXs} />
+                {iconSearchXs}
                 {itemValue}
               </Group>
             ))}
