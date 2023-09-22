e3e9229ffa4fe73a0c38737e5fdf9dd02e44b18a
황준혁 <strawji02@naver.com>
2023-09-22 15:00:11 +0900

    feat: :sparkles: add layout with route
    레이아웃 파일을 추가했습니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Suspense } from 'react';
 import { useRoutes } from 'react-router-dom';
 import routes from '~react-pages';
+import Layout from './layout';
 
 function App() {
-  return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>;
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <Layout>{useRoutes(routes)}</Layout>
+    </Suspense>
+  );
 }
 
 export default App;
